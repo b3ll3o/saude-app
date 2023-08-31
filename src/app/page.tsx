d745efd58112e6c3139455da2e06cea78bfd01b9
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2023-08-31 01:39:26 -0300

    ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <header className={styles.header}>
-        <h1>
-          AzziCare
-        </h1>
-        <nav>
-
-        </nav>
+        <h1>AzziCare</h1>
+        <nav></nav>
       </header>
       <main className={styles.main}>
         <p>Ola mundo!</p>
       </main>
-      <footer>
-        fotter
-      </footer>
+      <footer>fotter</footer>
     </>
-  )
+  );
 }
