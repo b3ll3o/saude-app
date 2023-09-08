e6a22c45ea48582511d946a96094049dcae8ee9d
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2023-09-08 15:31:17 -0300

    lint

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,29 +2,29 @@
 export default function Login() {
   return (
     <div className="w-full max-w-md">
-    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div className="mb-4">
-        <label className="block text-gray-700 text-sm font-bold mb-2" >
-          E-mail
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Digite seu e-mail"></input>
-        </label>
-      </div>
-      <div className="mb-6">
-        <label className="block text-gray-700 text-sm font-bold mb-2" >
-          Senha
-          <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
-        </label>
-        <p className="text-red-500 text-xs italic">Por favor coloque uma senha</p>
-      </div>
-      <div className="flex items-center justify-between">
-        <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-          Esqueceu a senha?
-        </a>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-          Entrar
-        </button>
-      </div>
-    </form>
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" >
+            E-mail
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Digite seu e-mail"></input>
+          </label>
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" >
+            Senha
+            <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"></input>
+          </label>
+          <p className="text-red-500 text-xs italic">Por favor coloque uma senha</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+            Esqueceu a senha?
+          </a>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            Entrar
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
